8d8176a7fa3273238f926ee8f21250f60483650a
youknowlx0-hash <youknowlx0@gmail.com>
2026-02-02 22:56:11 +0530

    Change BOT_TOKEN in config.py
    Updated the BOT_TOKEN to the new value.

---
+++ config.py
@@ -0,0 +1,18 @@
+BOT_TOKEN = "8183101684:AAGlpAwQDYMTOCVVBCre9LiZK82YQ1t7jl8"
+
+ADMINS = [7702942505]   # apna Telegram ID
+
+CHANNELS = [
+    "@Shein_Reward",
+    "@earnmoneysupport1",
+    "@GlobalTaskWorks",
+    "@Manish_Looterss"
+]
+
+# redeem rules (ONLY BALANCE)
+REDEEM_POINTS = {
+    "500": 3,
+    "1000": 6,
+    "2000": 10,
+    "4000": 15
+}
