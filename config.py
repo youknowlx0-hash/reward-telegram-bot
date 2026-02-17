c4432b493a0f019a683b4c10709ac2b38dea70f6
youknowlx0-hash <youknowlx0@gmail.com>
2026-02-17 23:47:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-ADMINS = [7702942505]
+BOT_TOKEN = "8516087399:AAGVAt0yw_4m1z8cqR5FR5ymNbWjlxXDwBI"
 
-CHANNELS = [
-    "@Shein_Reward",
+ADMINS = [7702942505]  # apna telegram ID daalo
+
+CHANNELS = ["@Shein_Reward",
     "@SheinStockss",
     "@sheinverse052",
     "@sheinlinks202",
-    "@SheinRewardsGc"
-]
+    "@SheinRewardsGc"]
 
-REDEEM_POINTS = {
-    500: 4,
-    1000: 10,
-    2000: 15,
-    4000: 20
+VOUCHER_COST = {
+    "500": 4,
+    "1000": 10,
+    "2000": 20,
+    "4000": 40
 }
